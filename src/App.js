ff27ec58a76f444735e3a2e1deec743b462047b9
seif <seiftarek090@gmail.com>
2025-05-12 16:27:30 +0300

    Refactor app structure and remove unused files; implement packing list functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Shoes", quantity: 1, packed: false },
+  { id: 4, description: "Toothbrush", quantity: 1, packed: true },
+];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(){
+  return(
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
-  );
+  )
 }
 
-export default App;
+function Logo(){
+  return(
+    <h1>🌴 Far Away 🌴</h1>
+  )
+}
+function Form(){
+  return(
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+      <form>
+        <input type="text" placeholder="Item..." />
+        <input type="number" placeholder="Quantity..." />
+        <button>Add</button>
+      </form>
+    </div>
+  )
+}
+function PackingList(){
+  return(
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  )
+}
+function Item({item}){
+  return (
+    <li>
+      <input type="checkbox" />
+      <span style={item.packed ? {textDecoration: "line-through"} : {}}>{item.quantity}{item.description}</span>
+      <button>❌</button>
+    </li>
+  )
+}
+function Stats(){
+  return(
+    <footer className="stats">
+      <p>👜You have x items on your list, and you already packed x (x%)</p>
+    </footer>
+  )
+}
