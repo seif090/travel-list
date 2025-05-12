94b2a7cd28295017584d917a3e539360984c8510
seif <seiftarek090@gmail.com>
2025-05-12 17:01:43 +0300

    Refactor App component structure and improve form handling

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -5,58 +7,84 @@ const initialItems = [
   { id: 4, description: "Toothbrush", quantity: 1, packed: true },
 ];
 
-export default function App(){
-  return(
+export default function App() {
+  return (
     <div className="app">
       <Logo />
       <Form />
       <PackingList />
       <Stats />
     </div>
-  )
+  );
 }
 
-function Logo(){
-  return(
-    <h1>🌴 Far Away 🌴</h1>
-  )
+function Logo() {
+  return <h1>🌴 Far Away 🌴</h1>;
 }
-function Form(){
-  return(
-    <div className="add-form">
+function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(0);
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+    const newPackingItem = {
+      description: e.target[0].value,
+      quantity: e.target[1].value,
+      packed: false,
+      id: Date.now(),
+    };
+    console.log(newPackingItem);
+    setDescription("");
+    setQuantity(0);
+  }
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-      <form>
-        <input type="text" placeholder="Item..." />
-        <input type="number" placeholder="Quantity..." />
-        <button>Add</button>
-      </form>
-    </div>
-  )
+      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num}>{num}</option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
 }
-function PackingList(){
-  return(
+function PackingList() {
+  return (
     <div className="list">
       <ul>
         {initialItems.map((item) => (
           <Item item={item} />
+        
         ))}
+
       </ul>
+      
     </div>
-  )
+  );
 }
-function Item({item}){
+function Item({ item }) {
   return (
     <li>
       <input type="checkbox" />
-      <span style={item.packed ? {textDecoration: "line-through"} : {}}>{item.quantity}{item.description}</span>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity}
+        {item.description}
+      </span>
       <button>❌</button>
     </li>
-  )
+  );
 }
-function Stats(){
-  return(
+function Stats() {
+  return (
     <footer className="stats">
       <p>👜You have x items on your list, and you already packed x (x%)</p>
     </footer>
-  )
+  );
 }
