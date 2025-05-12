29db001ad82b21afcbba876bde186cfdb8e9d102
seif <seiftarek090@gmail.com>
2025-05-12 23:16:52 +0300

    Enhance PackingList component with sorting functionality and toggle packed status

--- src/App.js
+++ src/App.js
@@ -71,14 +71,33 @@ function Form({ onAddItem }) {
     </form>
   );
 }
-function PackingList({ items, onDeleteItem }) {
+function PackingList({ items, onDeleteItem, onTogglePacked }) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems;
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
-          <Item item={item} key={item.id} onDeleteItem={onDeleteItem} />
+        {sortedItems.map((item) => (
+          <Item item={item} key={item.id} onDeleteItem={onDeleteItem} onTogglePacked={onTogglePacked} />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value='input'>Sort by Input</option>
+          <option value="description">Sort by Description</option>
+          <option value="packed">Sort by Packed Status</option>
+        </select>
+        <button>Clear List</button>
+      </div>
     </div>
   );
 }
